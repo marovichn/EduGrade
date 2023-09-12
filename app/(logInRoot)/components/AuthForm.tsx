0c75ea54e653d7ed13bf8b18c2128b6058dc1a08
marovichn <nikolamarovic.11@gmail.com>
2023-09-13 00:08:16 +0200

    Next-Auth, Prisma init, Mongo, AuthForm

---
+++ app/(logInRoot)/components/AuthForm.tsx
@@ -0,0 +1,13 @@
+import Variant from '@/types'
+import { FC } from 'react'
+
+
+interface AuthFormProps {
+  variant: Variant
+}
+
+const AuthForm: FC<AuthFormProps> = ({variant}) => {
+  return <div>AuthForm</div>
+}
+
+export default AuthForm
---
+++ types.ts
@@ -0,0 +1,3 @@
+type Variant = "LOGIN" | "REGISTER";
+
+export default Variant;

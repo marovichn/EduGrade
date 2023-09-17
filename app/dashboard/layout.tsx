4a570f3c9eb1e34dc90c1b00b45ca0c63fc11797
marovichn <nikolamarovic.11@gmail.com>
2023-09-17 18:05:21 +0200

    Dashboard UI - 3

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { MdCastForEducation } from "react-icons/md";
 
 export default function DashboardLayout({
@@ -10,12 +11,12 @@ export default function DashboardLayout({
     <>
       <nav className='bg-white dark:bg-black fixed w-full z-20 top-0 left-0 border-b border-0 dark:border-0 rounded-b-xl'>
         <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-10'>
-          <a href='/dashboard' className='flex items-center'>
+          <Link href='/dashboard' className='flex items-center'>
             <MdCastForEducation className='w-12 h-12 text-yellow-300 mr-4' />
             <span className='self-center text-3xl font-semibold whitespace-nowrap dark:text-white'>
               EduGrade
             </span>
-          </a>
+          </Link>
         </div>
       </nav>
       <div className='fixed top-16 z-15 bg-yellow-300 text-black w-full h-16 rounded-b-xl'></div>

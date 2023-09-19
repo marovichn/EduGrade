47cb7a4e7a85d790717220e4a745ddf83151b383
marovichn <nikolamarovic.11@gmail.com>
2023-09-20 00:47:21 +0200

    Student/Teacher Routes

---
+++ app/helpers/NavRoutes.ts
@@ -0,0 +1,30 @@
+import { IconNode, GraduationCap, User2, Group, BookCopy, CalendarCheck, Sticker, ListChecks} from "lucide-react";
+
+type navRoutes = [
+  {
+    href: string;
+    role: "Admin" | "Student" | "Teacher";
+    name: string;
+    icon: IconNode;
+  }
+];
+
+export const navRoutes = [
+  { href: "/students", role: "Admin", name: "Students", icon: User2 },
+  { href: "/teachers", role: "Admin", name: "Teachers", icon: GraduationCap },
+  { href: "/subjects", role: "Admin", name: "Subjects", icon: BookCopy },
+  { href: "/groups", role: "Admin", name: "Groups", icon: Group },
+
+  { href: "/updates", role: "Student", name: "All Grades", icon: User2 },
+  { href: "/results", role: "Student", name: "Subjects Grades", icon: ListChecks },
+  { href: "/activity", role: "Student", name: "Subjects Activities", icon: Sticker },
+  { href: "/attendance", role: "Student", name: "Attendance", icon: CalendarCheck },
+  { href: "/teachers", role: "Student", name: "My Teachers", icon: GraduationCap },
+  
+
+  { href: "/groups", role: "Teacher", name: "All Groups", icon: User2 },
+  { href: "/results", role: "Teacher", name: "Subjects Grades", icon: ListChecks },
+  { href: "/activity", role: "Teacher", name: "Subjects Activities", icon: Sticker },
+  { href: "/attendance", role: "Teacher", name: "Attendance", icon: CalendarCheck },
+  { href: "/students", role: "Teacher", name: "My Students", icon: GraduationCap },
+];

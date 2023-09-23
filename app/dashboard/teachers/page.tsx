d18d2f8f30d154c16de6e9b114092e8eedc81769
marovichn <nikolamarovic.11@gmail.com>
2023-09-23 20:47:18 +0200

    Teachers, teachers/create , UI

---
+++ app/dashboard/teachers/page.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+    <div>teacher1</div>
+  </div>
+}
+
+export default page

671142b5494693218c7ac352d45f15492feae268
marovichn <nikolamarovic.11@gmail.com>
2023-09-28 00:16:54 +0200

    (Teacher) Grade students form - 3

--- app/dashboard/(results)/create-results/components/ResultForm.tsx
+++ app/dashboard/(results)/create-results/components/ResultForm.tsx
@@ -11,6 +11,8 @@ import { toast } from "react-hot-toast";
 import PageWrapper from "@/app/components/PageWrapper";
 import { Group } from "@prisma/client";
 import SelectGroup from "./SelectGroup";
+import Select from "@/app/components/inputs/Select";
+import { grades } from "@/app/helpers/Grades";
 
 interface ResultsFormProps {
   userRole: string | null | undefined;
@@ -60,6 +62,7 @@ const ResultsForm: FC<ResultsFormProps> = ({ userRole, groups }) => {
           w-full
         '
         >
+          <h1 className='text-4xl font-bold mb-10'>Add Grade:</h1>
           <form className='space-y-6 w-full' onSubmit={handleSubmit(onSubmit)}>
             <SelectGroup
               name='group'
@@ -70,6 +73,21 @@ const ResultsForm: FC<ResultsFormProps> = ({ userRole, groups }) => {
               required
               id='role'
               label='Select Group (Student)'
+            />
+            <div>
+                <h1 className="mb-5">Select Grade:</h1>
+                <div className="w-full bg-gray-100 h-[104px] rounded-lg flex justify-around items-center gap-x-4">{grades.map((grade)=>(<div key={grade.key}  className="w-20 h-20 flex items-center justify-center rounded-md bg-white text-2xl
+                border-2 border-transparent hover:border-2 hover:border-yellow-300">{grade.value}</div>))}</div>
+            </div>
+            <Select
+              name='type'
+              options={["Exam", "Class Activity", "Group Project", "individual Project", "Other"]}
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id='type'
+              label='Select Type of Grade'
             />
           </form>
         </div>
---
+++ app/helpers/Grades.ts
@@ -0,0 +1,7 @@
+export const grades = [
+    {key: "A", value: "A"},
+    {key: "B", value: "B"},
+    {key: "C", value: "C"},
+    {key: "D", value: "D"},
+    {key: "F", value: "F"},
+]

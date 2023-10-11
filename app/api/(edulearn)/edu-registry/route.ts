213b6df6c9e6e5a269bf224f3e82e8953ce7be6c
marovichn <nikolamarovic.11@gmail.com>
2023-10-12 00:38:31 +0200

    EduConnection - 4 - server config errors

--- app/api/(edulearn)/edu-registry/route.ts
+++ app/api/(edulearn)/edu-registry/route.ts
@@ -1,7 +1,19 @@
 import prisma from "@/app/libs/prismadb";
+import { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+import Cors from "cors";
+import { runMiddleware } from "@/app/helpers/runMiddleware";
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify the allowed HTTP methods
+  origin: "https://localhost:3001", // Replace with your frontend domain
+});
+
+export async function POST(request: Request, res: NextApiResponse) {
+  await runMiddleware(request, res, cors);
+
   const { eduConnectionAccessToken } = await request.json();
   if (!eduConnectionAccessToken) {
     return new NextResponse("Unauthorized", { status: 401 });
@@ -10,7 +22,6 @@ export async function POST(request: Request) {
   if (eduConnectionAccessToken !== accessTokenValidator) {
     return new NextResponse("Unauthorized", { status: 401 });
   }
-
   const teachersData = await prisma.teacher.findMany({});
   const studentsData = await prisma.student.findMany({});
   const adminsData = await prisma.admin.findMany({});
@@ -20,5 +31,5 @@ export async function POST(request: Request) {
     studentsData: studentsData,
     adminsData: adminsData,
   };
-  return NextResponse.json(registryData);
+  return res.json(registryData);
 }
---
+++ app/helpers/runMiddleware.ts
@@ -0,0 +1,10 @@
+export function runMiddleware(req: any, res: any, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}

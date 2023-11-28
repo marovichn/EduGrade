6e97757e50b02fdfde3724149a20b4b85d4d91aa
marovichn <nikolamarovic.11@gmail.com>
2023-11-28 22:19:05 +0100

    EduConnection - 31 - Eduxclusive Courses - EduGrade - v.3.1.0 - Finish

--- app/api/(assignments)/assignments/route.ts
+++ app/api/(assignments)/assignments/route.ts
@@ -3,34 +3,40 @@ import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const currentUser = await getCurrentUser();
-  if (!currentUser) {
-    throw new NextResponse("Unauthorized", { status: 401 });
-  }
+  try {
+    const currentUser = await getCurrentUser();
 
-  const allMyGroups = await prisma.group.findMany({
-    where: {
-      studentId: currentUser.id,
-    },
-    include: { assignments: true },
-  });
+    if (!currentUser) {
+      throw new NextResponse("Unauthorized", { status: 401 });
+    }
 
-  if (allMyGroups.length === 0 || !allMyGroups) {
-    const allMyGroupsStudent = await prisma.group.findMany({
+    const allMyGroups = await prisma.group.findMany({
       where: {
         studentId: currentUser.id,
       },
-      include: {
-        assignments: true,
-      },
+      include: { assignments: true },
     });
 
-    let assignments = allMyGroupsStudent
-      .map((group) => group.assignments)
-      .flat();
+    let assignments = [];
+
+    if (allMyGroups.length > 0) {
+      assignments = allMyGroups.map((group) => group.assignments).flat();
+    } else {
+      const allMyGroupsStudent = await prisma.group.findMany({
+        where: {
+          studentId: currentUser.id,
+        },
+        include: {
+          assignments: true,
+        },
+      });
+
+      assignments = allMyGroupsStudent.map((group) => group.assignments).flat();
+    }
 
     return NextResponse.json(assignments);
+  } catch (error) {
+    console.error("Error during prerendering:", error);
+    return NextResponse.error();
   }
-  const assignments = allMyGroups.map((group) => group.assignments).flat();
-  return NextResponse.json(assignments);
 }
